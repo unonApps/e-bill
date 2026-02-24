6a58caa570a4aa91cc44d2fab1bc089d328099d7
dxmic <dxmic@gmail.com>
2026-02-24 12:07:20 +0300

    Add EF migration for covering indexes on CallLogPaymentAssignment and CallLogVerification

--- Migrations/20260224090625_AddCoveringIndexesForMyCallLogs.Designer.cs
+++ Migrations/20260224090625_AddCoveringIndexesForMyCallLogs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TAB.Web.Data;
 
@@ -11,9 +12,11 @@ using TAB.Web.Data;
 namespace TAB.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224090625_AddCoveringIndexesForMyCallLogs")]
+    partial class AddCoveringIndexesForMyCallLogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260224090625_AddCoveringIndexesForMyCallLogs.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAB.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoveringIndexesForMyCallLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CallLogVerifications_SubmittedToSupervisor_CallRecordId_ApprovalStatus",
+                schema: "ebill",
+                table: "CallLogVerifications",
+                columns: new[] { "SubmittedToSupervisor", "CallRecordId", "ApprovalStatus" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CallLogPaymentAssignments_AssignedFrom_AssignmentStatus_CallRecordId",
+                schema: "ebill",
+                table: "CallLogPaymentAssignments",
+                columns: new[] { "AssignedFrom", "AssignmentStatus", "CallRecordId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CallLogPaymentAssignments_AssignedTo_AssignmentStatus_CallRecordId",
+                schema: "ebill",
+                table: "CallLogPaymentAssignments",
+                columns: new[] { "AssignedTo", "AssignmentStatus", "CallRecordId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CallLogVerifications_SubmittedToSupervisor_CallRecordId_ApprovalStatus",
+                schema: "ebill",
+                table: "CallLogVerifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CallLogPaymentAssignments_AssignedFrom_AssignmentStatus_CallRecordId",
+                schema: "ebill",
+                table: "CallLogPaymentAssignments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CallLogPaymentAssignments_AssignedTo_AssignmentStatus_CallRecordId",
+                schema: "ebill",
+                table: "CallLogPaymentAssignments");
+        }
+    }
+}
